8f7b5e6e40f89a71d308aacb70cd4410f48529ed
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-01-19 20:20:45 +0530

    19th 1st

---
+++ printsumoffirstnnaturalnumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class printsumoffirstnnaturalnumbers{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+
+        while(i <= i ){
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum is = " + sum);
+    }
+}
