57598ec721a917e5476ea0a9a7e219679c6c5209
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-01-20 19:24:55 +0530

    do while loop

---
+++ doowhile.java
@@ -0,0 +1,10 @@
+public class doowhile{
+    public static void main(String args[]){
+        int i = 1;
+
+        do {
+            system.out.println("Hello world");
+            i++;
+        }while(i <10);
+    }
+}
