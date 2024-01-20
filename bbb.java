71b2d6135e350f808707e03620c69274d056a469
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-01-20 19:33:58 +0530

    break statement in loop

---
+++ bbb.java
@@ -0,0 +1,11 @@
+public class bbb{
+    public static void main(String args[]){
+    for(int i = 1; i <= 5; i ++){
+        if(i == 3){
+            break;
+        }
+        System.out.println(i);
+    }
+    System.out.println("I am out of the loop");
+}
+}
