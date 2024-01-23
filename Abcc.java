3e936928a988bc8b21d2e15fbe983cfb92c0c31a
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-01-23 19:11:23 +0530

    Half pyramid pattern.

---
+++ Abcc.java
@@ -0,0 +1,11 @@
+public class Abcc{
+    public static void main(String args[]){
+        int n=4;
+        for(int line = 1; line <=n; line++){
+            for(int number = 1; number<=line; number++){
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+    }
+}
