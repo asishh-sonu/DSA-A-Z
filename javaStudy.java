d6a35efd0447fe42dd8ffbd44f4fcbbff0953370
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-01-24 19:07:06 +0530

    Example of function

---
+++ javaStudy.java
@@ -0,0 +1,10 @@
+public class Study{
+    public static void print(){
+        System.out.println("Asish");
+        System.out.println("Asish");
+        System.out.println("Asish");
+    }
+    public static void main(String args[]){
+        print();
+    }
+}
