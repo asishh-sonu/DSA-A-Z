97036d9c4b1f3d8debcac349f732e4cbc39240b8
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-01-25 19:55:14 +0530

    swap numbers

---
+++ Swap.java
@@ -0,0 +1,12 @@
+public class Swap{
+    public static void main(String args[]){
+        int a = 5;
+        int b = 10;
+
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+    }
+}
