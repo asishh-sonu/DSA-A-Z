0aebef256e2e59bb9c06da8f88daeca9655a166f
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-01-26 19:24:49 +0530

    Find the product of a and b using method

---
+++ Animal.java
@@ -0,0 +1,13 @@
+public class Animal{
+    public static int multiply(int a, int b){
+        int product = a * b;
+        return product;
+    }
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        int prod = multiply(a,b);
+        System.out.println("a * b = " + prod);
+        
+    }
+}
