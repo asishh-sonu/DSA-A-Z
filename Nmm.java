7795bccdd361141105c820cd924d5405e7801c43
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-01-27 11:27:53 +0530

    Function overloading using data types

---
+++ Nmm.java
@@ -0,0 +1,12 @@
+public class Nmm{
+    public static int sum(int a, int b){
+        return a + b ;
+    }
+    public static float sum(float a, float b){
+        return a + b;
+    }
+    public static void main(String args[]){
+        System.out.println(sum(3,10));
+        System.out.println(sum(2.3f,2.0f));
+    }
+}
