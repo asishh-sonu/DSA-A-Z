3c09b91f2b0540d3e1b9617737e23276153d2d2e
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-02-01 20:33:46 +0530

    Array creation

---
+++ ArrayCC.java
@@ -0,0 +1,16 @@
+import java.util.*;
+    public class ArrayCC{
+        public static void main(String args[]){
+            int marks[] = new int [100];
+
+            Scanner sc = new Scanner(System.in);
+
+            marks[0] = sc.nextInt();
+            marks[1] = sc.nextInt();
+            marks[2] = sc.nextInt();
+
+            System.out.println("Maths : " + marks[0]);
+            System.out.println("Odia : " + marks[1]);
+            System.out.println("English : " + marks[2]);
+        }
+    }
