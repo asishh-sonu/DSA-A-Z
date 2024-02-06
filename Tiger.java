e2d1b99467bd8e51a4031eb70e209da20a092c2b
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-02-06 19:57:27 +0530

    Smallest and largest number in array

--- Tiger.java
+++ Tiger.java
@@ -2,16 +2,21 @@ import java.util.*;
 public class Tiger{
     public static int getLargest(int numbers[]){
         int largest = Integer.MIN_VALUE; //- Infinity
+        int smallest = Integer.MAX_VALUE;
 
         for(int i=0; i<numbers.length; i++){
             if(largest<numbers[i]){
                 largest=numbers[i];
             }
+            if(smallest>numbers[i]){
+                smallest=numbers[i];
+            }
         }
+        System.out.println("Smallest number is : " + smallest);
         return largest;
     }
     public static void main(String args[]){
-        int numbers[] = {1,12,25,6,8,50};
+        int numbers[] = {10,12,25,6,8,50};
         System.out.println("Largest number is " + getLargest(numbers));
     }
 }
