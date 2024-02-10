f6f7002045922a10a897e9a1541e8f30cf7f0a98
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-02-10 22:08:26 +0530

    Totall pairs in the array

--- College.java
+++ College.java
@@ -1,13 +1,16 @@
 public class College {
     public static void printpairs(int numbers[]) {
+        int tp = 0;
         for (int i = 0; i < numbers.length; i++) {
             int curr = numbers[i];
             for (int j = i + 1; j < numbers.length; j++) {
                 System.out.print("(" + curr + ", " + numbers[j] + ") ");
+                tp++;
               
             }
             System.out.println();
         }
+        System.out.println("Totall pairs = " + tp);
     }
 
     public static void main(String args[]) {
@@ -15,3 +18,4 @@ public class College {
         printpairs(numbers);
     }
 }
+
