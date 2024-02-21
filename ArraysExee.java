ee3b5fa3b599c4fdb321a8261d3631d6ac993cf8
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-02-21 21:29:39 +0530

    Not found added

--- ArraysExee.java
+++ ArraysExee.java
@@ -1,7 +1,7 @@
 public class ArraysExee{
     void searchInArray(){
         int arr[] = {1,20,3,5,4,7};
-        int x = 4;
+        int x = 22;
         int ans = -1;
 
         for(int i = 0; i <arr.length; i++ ){
@@ -9,8 +9,11 @@ public class ArraysExee{
                 ans = i;
             }
         }
+        if(ans == -1){
+            System.out.println("Not found");
+        } else {
         System.out.println("Found " + x + " at index at " + ans);
-
+        }
     }
     public static void main(String args[]){
         ArraysExee obj = new ArraysExee();
