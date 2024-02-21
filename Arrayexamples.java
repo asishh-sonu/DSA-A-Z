b1de6241aa1491aceb8fba06431289f6689dc536
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-02-21 20:11:56 +0530

    Calculate the sum of all the elemets in the given array.

---
+++ Arrayexamples.java
@@ -0,0 +1,14 @@
+public class Arrayexamples{
+    void sumofArray(){
+        int arr[] = {1,5,3,7,8};
+        int sum = 0;
+        for(int i = 0; i<arr.length; i++){
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+    public static void main(String args[]){
+        Arrayexamples obj = new Arrayexamples();
+        obj.sumofArray();
+    }
+}
