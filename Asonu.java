4f7dbb0c9a98eeb28b98db1e5fe47cabc356bd8a
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-02-27 12:32:29 +0530

    Print max Sub array Sum-1 (Brout force)

---
+++ Asonu.java
@@ -0,0 +1,27 @@
+public class Asonu{
+    public static void maxSubArraySum(int numbers[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i = 0; i<numbers.length; i++){
+            int start = i;
+            
+            for(int j = i; j<numbers.length; j++){
+                int end = j;
+                currSum = 0;
+                for(int k = start; k<=end; k++){
+                    currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                if(maxSum<currSum){
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("max sum = " + maxSum);
+    }
+    public static void main(String args[]){
+        int number[] = {2,4,6,8,10};
+        maxSubArraySum(number);
+}
+}
