a3db397a38696418187605c9b37038bf9d955a01
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-02-27 11:45:09 +0530

    Print sunarrays - total sub arrays added

--- Clesd.java
+++ Clesd.java
@@ -1,16 +1,19 @@
 public class Clesd{
     public static void PrintSubarrays(int numbers[]){
+        int ts = 0;
         for(int i = 0; i<numbers.length; i++){
             int start = i;
             for(int j = i; j<numbers.length; j++){
                 int end = j;
                 for(int k = start; k<= end; k++){
                     System.out.print(numbers[k] + " ");
+                    ts++;
                 }
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println("Totall Subarray = " + ts);
     }
     public static void main(String args[]){
         int numbers [] = {2,4,6,8,10};
