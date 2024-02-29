279d5eef5f29cb2755cc3537007a9925a1b7ec5d
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-02-29 19:08:00 +0530

    Start pattern

---
+++ StartP.java
@@ -0,0 +1,10 @@
+public class StartP{
+    public static void main(String args[]){
+        for(int i = 1; i<=3; i++){
+            for(int j = 1; j <=6; j++ ){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
