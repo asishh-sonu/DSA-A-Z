c25d36e1262a8ea330c2bb8fb97db656f00c1307
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-03 17:42:22 +0530

    ContainerWithMostWater

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,28 @@
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int currentArea = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        int maximumArea = maxArea(heights);
+
+        System.out.println("Maximum area of water the container can hold: " + maximumArea);
+    }
+}
