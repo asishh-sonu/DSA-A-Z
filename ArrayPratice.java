8f02d012353f02343eabf44762d1ef5d467e3dd1
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-04 20:23:44 +0530

    Array Pratice

---
+++ ArrayPratice.java
@@ -0,0 +1,14 @@
+public class ArrayPratice{
+    public static void main(String args[]){
+        char[] a = new char[5];
+        a[0] = 'a';
+        a[1] = 'b';
+        a[2] = 'c';
+        a[3] = 'd';
+        a[4] = 'e';
+
+        for(int i = 0; i<5; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
