21725e8d7557b63f692c72d3aa38dd2f5f02a5f4
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-04 20:29:25 +0530

    Array printing pratice

---
+++ Virat.java
@@ -0,0 +1,11 @@
+public class Virat{
+    public static void main(String args[]){
+        int [] a = new int [10];
+        for(int i = 0; i<10; i++){
+            a[i]= i ;
+        }
+        for(int i = 0; i<10; i++){
+            System.out.println(a[i]);
+        }
+    }
+}
