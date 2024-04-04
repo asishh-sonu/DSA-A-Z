2c8de00b907f6c29ef2b0314d46391307071ac2d
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-04 18:05:24 +0530

    Pratice

---
+++ Learning.java
@@ -0,0 +1,14 @@
+public class Learning{
+    public static void main (String args[]){
+
+    int x = 20;
+    int y = 30;
+
+    if(x>y){
+        System.out.println("X is greater than Y");
+    }else 
+    {
+        System.out.println("Y is greater than X");
+    }
+}
+}
