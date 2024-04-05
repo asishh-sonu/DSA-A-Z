6066a53be5f4821acc98c8a886102616620a0c09
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-05 22:24:28 +0530

    Addition using function

---
+++ SRH.java
@@ -0,0 +1,13 @@
+public class SRH{
+    public static void CSK(int a , int b){
+        System.out.println(a+b);
+
+    }
+
+    public static void main(String args[]){
+        int a = 10; 
+        int b = 30;
+
+        CSK(a,b);
+    }
+}
