53fca4f99f0191bd0b1e52d53befcd80996d7e33
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-05 22:14:22 +0530

    Function Pratice

---
+++ FunctionPratice.java
@@ -0,0 +1,10 @@
+public class FunctionPratice{
+    public static void main(String args[]){
+
+        Pal();
+    }
+
+    public static void Pal() {
+        System.out.println("CSK is going to loose today");
+    }
+}
