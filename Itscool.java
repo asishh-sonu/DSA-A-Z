0ff289efd6b49f69e9c87d76587115199011fcd8
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-06 19:01:21 +0530

    First Unique Character in a String

---
+++ Itscool.java
@@ -0,0 +1,19 @@
+public class Itscool {
+    public static void main(String[] args) {
+        String s = "leetcode";
+        System.out.println(firstUniqChar(s));
+    }
+
+    public static int firstUniqChar(String s) {
+        int[] charCounts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            charCounts[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (charCounts[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
