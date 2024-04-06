bdcc03a3181c5b025aa027f85516ff22873eb4ca
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-06 19:00:06 +0530

    Search Insert Position

---
+++ Machine.java
@@ -0,0 +1,23 @@
+class Machine {
+   
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        System.out.println(searchInsert(nums, target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
