0500f28c5f4e06461b03a7e389edaa2d207edc19
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-06 18:57:22 +0530

    Find First and Last Position of Element in Sorted Array

---
+++ Washing.java
@@ -0,0 +1,47 @@
+public class Washing {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+        int[] result = searchRange(nums, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    public static int findFirst(int[] nums, int target){
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] >= target){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+            if(nums[mid] == target) idx = mid;
+        }
+        return idx;
+    }
+
+    public static int findLast(int[] nums, int target){
+        int idx = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] <= target){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+            if(nums[mid] == target) idx = mid;
+        }
+        return idx;
+    }
+}
