9c22dc03b78a9e7e2d9f3e2ad40b7eefdade5986
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-09 20:41:39 +0530

    Get elemets from the array list

--- LearnArrayList.java
+++ LearnArrayList.java
@@ -16,12 +16,14 @@ public class LearnArrayList{
         list.add(2, 20);
         System.out.println(list);
 
-        List<Integer> newList = new ArrayList();
+        List<Integer> newList = new ArrayList();   //creating a new array list
         newList.add(150);
         newList.add(200);
 
-        list.addAll(newList);
-        System.out.println(list);
+        list.addAll(newList);  //This will add all the elements in the list
+        System.out.println(list); 
+
+        System.out.println(list.get(3));  //get elemets from array list
 
     }
 }
