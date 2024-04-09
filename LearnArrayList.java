8a88b59ec681165a7f65e982b0525bc78d5aa994
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-09 18:39:48 +0530

    Learning  ArrayList

---
+++ LearnArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LearnArrayList{
+    public static void main(String args[]){
+        List<Integer> list = new ArrayList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+    }
+}
