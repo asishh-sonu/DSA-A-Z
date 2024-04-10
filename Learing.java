ff8cc5e88affebe8f334ff254c0ea54be85a4c45
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-10 23:08:17 +0530

    solo project

--- Learing.java
+++ Learing.java
@@ -4,6 +4,8 @@ public class Learing{
         System.out.println("CSK won");
         System.out.println("CSK won 3rd match");
         System.out.println("CSK won once again");
+        System.out.println("today i am doing my first solo project");
+        
 
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
