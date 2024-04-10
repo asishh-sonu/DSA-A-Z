f717cc7dbbb9af18cc5cdfbb86304c4d1f43c5d3
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-10 23:07:26 +0530

    ignore

---
+++ Learing.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Learing{
+    public static void main(String args[]){
+        System.out.println("CSK won");
+        System.out.println("CSK won 3rd match");
+        System.out.println("CSK won once again");
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println("CSK won once again");
+
+    }
+}
