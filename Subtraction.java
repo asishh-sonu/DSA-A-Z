c0d27bfb15329205d93c4bdbd3e74e5ecb598252
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-12 21:33:48 +0530

    Subtraction

---
+++ Subtraction.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Subtraction {
+	public static void main(String[] args)
+	{
+		
+		int num1 = 20, num2 = 10, sub = 0;
+
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		sub = num1 - num2;
+		System.out.println("Subtraction = " + sub);
+	}
+}
