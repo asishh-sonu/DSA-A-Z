754e2d60a32d6f978019cc3c035f6f0b673650b0
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-12 21:34:49 +0530

    Multiplication

--- Multiplication.java
+++ Multiplication.java
@@ -1,15 +1,14 @@
 import java.io.*;
 
-class Subtraction {
+class Multiplication {
 	public static void main(String[] args)
 	{
-		
-		int num1 = 20, num2 = 10, sub = 0;
+		int num1 = 20, num2 = 10, mult = 0;
 
 		System.out.println("num1 = " + num1);
 		System.out.println("num2 = " + num2);
-        
-		sub = num1 - num2;
-		System.out.println("Subtraction = " + sub);
+
+		mult = num1 * num2;
+		System.out.println("Multiplication = " + mult);
 	}
 }
