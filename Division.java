9401423f1a71b12d25a4b99a380eaa1e9aa810f4
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-12 21:35:48 +0530

    Division

---
+++ Division.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Division {
+	public static void main(String[] args)
+	{
+		
+		int num1 = 20, num2 = 10, div = 0;
+
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		div = num1 / num2;
+		System.out.println("Division = " + div);
+	}
+}
