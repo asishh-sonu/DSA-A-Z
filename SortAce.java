b8e25304cfd09729dd57f25a6bfd7380f1acffe7
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-13 22:39:43 +0530

    Sort the array in Descending oder

--- SortAce.java
+++ SortAce.java
@@ -11,7 +11,7 @@ public class SortAce{
 
         for(int i = 0;i<arr.length;i++){
             for(int j = i+1; j<arr.length;j++){
-                if(arr[i]>arr[j]){
+                if(arr[i]<arr[j]){
                     temp = arr[i];
                     arr[i] = arr[j];
                     arr[j] = temp;
