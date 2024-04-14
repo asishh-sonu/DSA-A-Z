16d45b233bdf07dcbb1bbb5b917aaf7ed37430bc
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-15 00:00:46 +0530

    Taking the input in String

--- StringLearning.java
+++ StringLearning.java
@@ -2,7 +2,13 @@ import java.util.*;
 
 public class StringLearning{
     public static void main(String args[]){
-        String str = "My name is asish";
-        System.out.println(str);
+        // String str = "My name is asish";
+        // System.out.println(str);
+
+        System.out.print("Write the String here : " );
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        
+        System.out.println(s);
     }
 }
