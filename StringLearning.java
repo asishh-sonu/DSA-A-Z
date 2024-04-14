d14ba336bc1076ab475f5b340a42d01427afe70b
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-14 23:54:47 +0530

    Declaration of String

---
+++ StringLearning.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class StringLearning{
+    public static void main(String args[]){
+        String str = "My name is asish";
+        System.out.println(str);
+    }
+}
