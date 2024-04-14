d1cb01cf588a9b38a69048dd9c6d6b8272d2a8bd
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-15 00:08:11 +0530

    Length of String

--- StringLearning.java
+++ StringLearning.java
@@ -5,10 +5,18 @@ public class StringLearning{
         // String str = "My name is asish";
         // System.out.println(str);
 
-        System.out.print("Write the String here : " );
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        // System.out.print("Write the String here : " );
+        // Scanner sc = new Scanner(System.in);
+        // String s = sc.nextLine();
         
-        System.out.println(s);
+        // System.out.println(s);
+
+        //String length
+        String str = "Asish";
+        System.out.println(str.length());
+
+        //char present at
+
+       
     }
 }
