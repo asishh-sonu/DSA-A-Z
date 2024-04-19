310b7d3e2acc690e3fe7fb4cd18e6f3dd3da1f47
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-19 21:33:14 +0530

    java pratice code

---
+++ MAhiiii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class MAhiiii{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter s :");
+        int s = sc.nextInt();
+        System.err.print("Enter s :");
+        int p = sc.nextInt();
+        int sum = s + p;
+        System.out.println("enter the values: " + sum);
+
+    }
+}
