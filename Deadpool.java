46c6d97150ae7758da82d60ae02e5a2539dd01ad
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-04-22 22:10:13 +0530

    Dont know

---
+++ Deadpool.java
@@ -0,0 +1,11 @@
+
+import java.io.*;
+
+class Deadpool {
+	public static void main (String[] args) {
+	String str="Knowledge";
+	String s=str;
+	str=str.concat(" Base");
+	System.out.println(str);
+	}
+}
