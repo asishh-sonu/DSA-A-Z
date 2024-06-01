7618463246c2995343fa6e86c5c177d2fb39f40f
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-01 19:31:39 +0530

    remove the 2nd index

--- LearnCollectionFrameWork.java
+++ LearnCollectionFrameWork.java
@@ -4,26 +4,42 @@ import java.util.List;
 public class LearnCollectionFrameWork{
     public static void main(String args[]){
      
+        // List<Integer> list = new ArrayList();
+        // list.add(1);
+        // list.add(2);
+        // list.add(3);
+        // System.out.println(list);
+
+        // list.add(4);  
+        // System.out.println(list);
+
+        // list.add(1,50);   // 50 added in the 1st index
+        // System.out.println(list);
+
+        // List<Integer> newList = new ArrayList(); // new list added
+        // newList.add(100);
+        // newList.add(200);
+
+        // list.addAll(newList);  // combination of two lists
+        // System.out.println(list);
+
+        // System.out.println(list.get(2));  // get an element from the list
+
         List<Integer> list = new ArrayList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        list.add(130);
+        list.add(100);
+
         System.out.println(list);
 
-        list.add(4);  
-        System.out.println(list);
-
-        list.add(1,50);   // 50 added in the 1st index
-        System.out.println(list);
-
-        List<Integer> newList = new ArrayList(); // new list added
-        newList.add(100);
-        newList.add(200);
-
-        list.addAll(newList);  // combination of two lists
-        System.out.println(list);
-
-        System.out.println(list.get(2));  // get an element from the list
+        list.remove(2);   // remove the 2nd index
+        System.out.println(list);  
 
 
         
