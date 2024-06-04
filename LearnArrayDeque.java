a94783e2710931248ba94fe50d4c90f8360ccdd6
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-04 22:12:15 +0530

    poll last

--- LearnArrayDeque.java
+++ LearnArrayDeque.java
@@ -7,12 +7,21 @@ public class LearnArrayDeque{
         adq.offer(20);
         adq.offerFirst(10);
         adq.offerLast(30);
+        adq.offer(56);
+        adq.offer(85);
         System.out.println(adq); //adding
+
+        System.out.println(adq.peek());
+        System.out.println(adq.peekFirst());
+        System.out.println(adq.peekLast());
 
         System.out.println(adq.poll());
         System.out.println("poll()" + adq); //poll
         
         System.out.println(adq.pollFirst());
         System.out.println("pollFirst()" + adq);
+
+        System.out.println(adq.pollLast());
+        System.out.println("PollLast()" +adq); //poll last
     }
 }
