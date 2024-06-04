50510676913361b2c21b05cd54846ab250d78063
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-04 19:43:25 +0530

    min heap

---
+++ LearnPriorityQueue.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+public class LearnPriorityQueue{
+    public static void main(String args[]){
+        Queue<Integer> pq = new PriorityQueue<>();
+        pq.offer(40);
+        pq.offer(12);
+        pq.offer(8);
+        pq.offer(60);
+        pq.offer(44);
+        pq.offer(66);
+
+        System.out.println(pq); //min heap
+
+    }
+}
