de630f1aea384a6cc7b2fdc1836febe4b0a33a83
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-05 19:54:24 +0530

    LearnCollections

---
+++ LearnCollections.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LearnCollections{
+    public static void main(String args[]){
+        List<Integer> list = new ArrayList<>();
+        list.add(34);
+        list.add(12);
+        list.add(9);
+        list.add(94);
+        list.add(9);
+        list.add(76);
+        list.add(29);
+        System.out.println("min element " + Collections.min(list));
+        System.out.println("Max element " + Collections.max(list));
+    }
+}
