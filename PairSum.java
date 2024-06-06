e8c48f8cb54ebeb62e41642d35d072f44094a15a
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-06 22:33:59 +0530

    PairSum

---
+++ PairSum.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class PairSum{
+    static int pairSum(int [] arr, int target){
+        int n = arr.length;
+        int ans = 0;
+        for(int i=0; i<n;i++){ //first number
+            for(int j=i+1; j<n;i++){ //second number
+                if(arr[i] + arr[j] == target){
+                    ans++;
+                }
+
+            }
+
+        }
+        return ans;
+    }
+    
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array :");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter"  + n + "elemnts");
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("Enter target sum");
+        int target = sc.nextInt();
+        System.out.println(pairSum(arr, target));
+    }
+}
