3fad83f7038f6caeeb04d7d38da29045c290eb53
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-06 22:47:00 +0530

    Learn PairSum

--- PairSum.java
+++ PairSum.java
@@ -6,7 +6,7 @@ public class PairSum{
         int n = arr.length;
         int ans = 0;
         for(int i=0; i<n;i++){ //first number
-            for(int j=i+1; j<n;i++){ //second number
+            for(int j=i+1; j<n;j++){ //second number
                 if(arr[i] + arr[j] == target){
                     ans++;
                 }
@@ -19,15 +19,16 @@ public class PairSum{
     
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array :");
+        System.out.print("Enter the size of array :");
         int n = sc.nextInt();
         int[] arr = new int[n];
-        System.out.println("Enter"  + n + "elemnts");
+        System.out.println("Enter "  + n + " elemnts");
         for(int i=0;i<n;i++){
             arr[i]= sc.nextInt();
         }
         System.out.println("Enter target sum");
         int target = sc.nextInt();
-        System.out.println(pairSum(arr, target));
+        System.out.println("Number of pairs with sum " + target + ": " + pairSum(arr, target));
+
     }
 }
