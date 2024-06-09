625721e0b849d859146912c05d27ce7ab0760208
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-09 19:50:51 +0530

    Bubble Sort

---
+++ BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class BubbleSort{
+    public static void bubblesort(int arr[]){
+        for(int turn = 0;turn<arr.length-1;turn++){
+            for(int j=0;j<arr.length-1-turn;j++){
+                if(arr[j]>arr[j+1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[ ] = {4,5,3,1,2};
+        bubblesort(arr);
+        printArr(arr);
+    }
+}
