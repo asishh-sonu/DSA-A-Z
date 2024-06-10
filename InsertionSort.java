483445e56d5c4cf89e130acb7b53aa1a5becb068
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-10 12:20:35 +0530

    insertionsort

--- InsertionSort.java
+++ InsertionSort.java
@@ -14,17 +14,18 @@ public class InsertionSort{
             }
         }
     }
-}
-
-public static void printArr(int arr[]) {
-    for (int i = 0; i < arr.length; i++) {
-        System.out.print(arr[i] + " ");
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
-    System.out.println();
+    
+    public static void main(String args[]){
+        int arr[] = {5,4,3,2,1};
+        insertionsort(arr);
+        printArr(arr);
+    }
 }
 
-public static void main(String args[]){
-    int arr = {5,4,3,2,1};
-    insertionsort(arr);
-    printArr(arr);
-}
+
