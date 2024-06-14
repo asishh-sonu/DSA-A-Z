f07cb5ee3810a9c511e6b5e0f97cd43958139bfe
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-14 11:43:54 +0530

    String length

--- StringExample.java
+++ StringExample.java
@@ -2,15 +2,18 @@ import java.util.*;
 
 public class StringExample{
     public static void main(String args[]){
-        String str="Hello";
-        System.out.println(str);
-        String str2 = new String("Hello asish");
-        System.out.println(str2);
+        // String str="Hello";
+        // System.out.println(str);
+        // String str2 = new String("Hello asish");
+        // System.out.println(str2);
 
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Enter the string:");
-        String s = sc.nextLine();
-        System.out.println("The entered string:");
-        System.out.println(s);
+        // Scanner sc= new Scanner(System.in);
+        // System.out.println("Enter the string:");
+        // String s = sc.nextLine();
+        // System.out.println("The entered string:");
+        // System.out.println(s);
+
+        String name = "Asish Kuamr Barik";
+        System.out.println(name.length());
     }
 }
