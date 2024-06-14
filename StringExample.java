b0ae1e064b0945a96bac626dbefea0d0af073f59
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-14 11:31:46 +0530

    String printing

---
+++ StringExample.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class StringExample{
+    public static void main(String args[]){
+        String str="Hello";
+        System.out.println(str);
+        String str2 = new String("Hello asish");
+        System.out.println(str2);
+    }
+}
