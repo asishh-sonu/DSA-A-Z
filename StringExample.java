cbfbc83110b3f0895726bf89bca689c5709d0108
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-14 11:40:57 +0530

    taking user input from string

--- StringExample.java
+++ StringExample.java
@@ -6,5 +6,11 @@ public class StringExample{
         System.out.println(str);
         String str2 = new String("Hello asish");
         System.out.println(str2);
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String s = sc.nextLine();
+        System.out.println("The entered string:");
+        System.out.println(s);
     }
 }
