29af8ae7cdd9fd7423ea6caca3621d2729a8c9d9
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-16 20:12:43 +0530

    Even or odd checking in Bit manipulation

--- Bitmanipulation.java
+++ Bitmanipulation.java
@@ -1,12 +1,24 @@
 import java.util.*;
 public class Bitmanipulation{
+    public static void OddOrEven(int n){
+        int bitMask=1;
+        if((n & bitMask) == 0){
+            System.out.println("even number");
+        } else {
+            System.out.println("Odd number");
+        }
+    }
     public static void main(String args[]){
-        System.out.println((5&6));
-        System.out.println((5|6));
-        System.out.println((5 ^ 6));
-        System.out.println((~5));
-        System.out.println((6>>1));
-        System.out.println((5<<2));
+        // System.out.println((5&6));
+        // System.out.println((5|6));
+        // System.out.println((5 ^ 6));
+        // System.out.println((~5));
+        // System.out.println((6>>1));
+        // System.out.println((5<<2));
+
+        OddOrEven(3);
+        OddOrEven(11);
+        OddOrEven(14);
         
     }
 }
