18ae442d1a0cc12a76c7c9f03c9c58d2095ed195
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-16 19:43:15 +0530

    Binary OR |

--- Bitmanipulation.java
+++ Bitmanipulation.java
@@ -2,5 +2,9 @@ import java.util.*;
 public class Bitmanipulation{
     public static void main(String args[]){
         System.out.println((5&6));
+        System.out.println((5|6));
+        // System.out.println((5&6));
+        // System.out.println((5&6));
+        
     }
 }
