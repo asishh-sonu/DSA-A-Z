48d747bb7ab06c28008441570751a6c177cd13ae
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-17 21:47:06 +0530

    Fast Exponentiation

---
+++ FastExponentiation.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class FastExponentiation {
+    public static int fastExpo(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(fastExpo(3, 5));  // Output: 243
+    }
+}
