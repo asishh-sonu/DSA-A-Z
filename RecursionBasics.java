de044f7b0fce8736bcefb90038a9f60b6cfc4612
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-06-26 21:40:57 +0530

    Print numbers from 1 to n

--- RecursionBasics.java
+++ RecursionBasics.java
@@ -7,8 +7,17 @@ public class RecursionBasics{
         System.out.print(n+" ");
         printDec(n-1);
     }
+    public static void printInc(int n){
+        if(n == 1){
+            System.out.print(n + " ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n + " ");
+    }
     public static void main(String args[]){
         int n = 10;
-        printDec(n);
+        // printDec(n);
+        printInc(n);
     }
 }
