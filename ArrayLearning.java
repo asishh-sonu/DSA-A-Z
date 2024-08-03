c30cc8a4554ab18ddcc9ec6dd1d83fca275fed0f
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-08-03 20:23:47 +0530

    Percentage

--- ArrayLearning.java
+++ ArrayLearning.java
@@ -12,5 +12,8 @@ class ArrayLearning{
         System.out.println("Maths : " + marks[0]);
         System.out.println("Odia : " + marks[1]);
         System.out.println("English : " + marks[2]);
+
+        int percentage = (marks[0] + marks[1] + marks[2])/3;
+        System.out.println("Percemtage : " + percentage + "%");
     }
 }
