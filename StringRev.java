7bb0fb28ab1e55e37c5252e73b51041a80f8ba51
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-08-15 22:47:46 +0530

    String Length

---
+++ StringRev.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class StringRev{
+    public static void main(String args[]){
+        String fullname = "Asish Kumar Barik";
+        System.out.println(fullname.length());
+    }
+}
