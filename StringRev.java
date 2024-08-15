649042a9f03d752ebef100d0313df23e8a987f20
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-08-15 23:10:31 +0530

    String concatenate

--- StringRev.java
+++ StringRev.java
@@ -3,5 +3,10 @@ class StringRev{
     public static void main(String args[]){
         String fullname = "Asish Kumar Barik";
         System.out.println(fullname.length());
+
+        String firstname = "asish";
+        String lastname = "kumar Barik";
+        String fname = firstname + " " + lastname;
+        System.out.print(fname);
     }
 }
