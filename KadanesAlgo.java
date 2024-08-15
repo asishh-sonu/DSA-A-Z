337c276fe60feb540a0edaf52535b32768b51367
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-08-15 22:37:33 +0530

    String learing

--- KadanesAlgo.java
+++ KadanesAlgo.java
@@ -20,3 +20,8 @@ public class KadanesAlgo{
         Kadans(numbers);
     }
 }
+
+
+
+
+
---
+++ StringLearing.java
@@ -0,0 +1,10 @@
+class StringLearing{
+    public static void main(String args[]){
+        char arr[] ={'a','b','c'};
+        String str = "abcd";
+        String str2 = new String("xyx");
+        
+        System.out.println(str2);
+        System.out.println(str);
+    }
+}
