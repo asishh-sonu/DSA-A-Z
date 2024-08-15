1a8adc6adfda4bd99a496717d8fa9a71330f2f5e
Asish Kumar Barik <asishsonu2003@gmail.com>
2024-08-15 22:41:08 +0530

    taking Input in String

--- StringLearing.java
+++ StringLearing.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class StringLearing{
     public static void main(String args[]){
         char arr[] ={'a','b','c'};
@@ -6,5 +7,12 @@ class StringLearing{
         
         System.out.println(str2);
         System.out.println(str);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string:");
+        String name;
+        name = sc.next();
+        System.out.print("The string is :");
+        System.out.println(name);
     }
 }
